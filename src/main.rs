b19d0e6bff96a700e66c33ff2750d73a9ac491ea
Hugh Brown <hughdbrown@yahoo.com>
2023-08-07 18:14:59 -0600

    Make sure ranges are typed

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 
 fn recursive_factorial(n: i64) -> i64 {
-    if n <= 1 { 1i64 } else {n * recursive_factorial(n - 1)}
+    if n <= 1i64 { 1i64 } else {n * recursive_factorial(n - 1)}
 }
 
 fn iterative_factorial(n: i64) -> i64 {
@@ -13,12 +13,12 @@ fn iterative_factorial(n: i64) -> i64 {
 
 fn main() {
     println!("Recursive");
-    for i in 0..21 {
-        println!("{i}: factorial({i}) = {}", recursive_factorial(i as i64));
+    for i in 0i64..21i64 {
+        println!("{i}: factorial({i}) = {}", recursive_factorial(i));
     }
 
     println!("Iterative");
-    for i in 0..21 {
-        println!("{i}: factorial({i}) = {}", iterative_factorial(i as i64));
+    for i in 0i64..21i64 {
+        println!("{i}: factorial({i}) = {}", iterative_factorial(i));
     }
 }
